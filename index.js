da72f90fc8aeffaf80c8ea6e7866e84b3874d604
Pedro Belo <pedrobelo@gmail.com>
2016-11-19 15:30:08 -0800

    remove browser stuff

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const {app, Tray, Menu, BrowserWindow} = require('electron');
+const {app, Tray, Menu} = require('electron');
 const path = require('path');
 const Mixable = require("./mixable.js");
 
 const iconPath = path.join(__dirname, 'icon.png');
 let appIcon = null;
-let win = null;
 
 app.on('ready', function(){
   Mixable.run();
 
-  win = new BrowserWindow({show: false});
   appIcon = new Tray(iconPath);
   var contextMenu = Menu.buildFromTemplate([
     {
